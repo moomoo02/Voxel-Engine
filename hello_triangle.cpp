4d70a9aa4b6624ca3f1ef5b0aa37661e6c8afdcb
Vince Nguyen <vincehnguyen@ucsb.edu>
2022-11-25 11:31:19 -0800

    Added fragment shader and linking

--- hello_triangle.cpp
+++ hello_triangle.cpp
@@ -1,54 +1,77 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+//Shader in GLSL
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
 
-//Takes in window, and new width and height.
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{   
-    glViewport(0, 0, width, height);
-};
+const char *fragmentShaderSource =
+"#version 400\n"
+"out vec4 frag_colour;"
+"void main() {"
+"  frag_colour = vec4(1.0f, 0.5f, 0.2f, 1.0);"
+"}";
 
-int main()
-{
-    //Initialize GLFW and configure using Hint
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+int main(){
+    //Vertices of a triangle
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };  
+
+    //Create a Vertex Buffer Object (VBO) that can store large number of vertices.
+    //Use glGenBuffers to generate buffer of id 1 and bind it to array buffer
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);  
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    //Copy data to buffer
+    /* Third Parameters ENUMS
+    GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
+    GL_STATIC_DRAW: the data is set only once and used many times.
+    GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
+    */
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
+
+
+    //Compile vertext shader object
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    //Check if vertex shader successfully compiled
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {   
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    //Compile fragment Shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
     
-    //Create a window object
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
+    //Link Shaders to shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::LINKING_FAILED\n" << infoLog << std::endl;
     }
-    glfwMakeContextCurrent(window);
-
-    //Make sure GLAD is initialized and working
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }     
-
-    //Specify viewport so opengl knows how to display data and coordinates with respect to the window
-    glViewport(0, 0, 800, 600);
-     
-    //Everytime window is resized, callback function to edit viewport.
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
-
-    //Render loop
-    while(!glfwWindowShouldClose(window)) //Checks if GLFW has been instructed to close
-    {
-        glfwSwapBuffers(window); //swap color buffer that is used to render 
-        glfwPollEvents(); //Checks if any events has been triggered, updates window states, and calls cooresponding functions
-    }
-
-    //Clean up all of GLFW resources
-    glfwTerminate();
-
-    return 0;
 }
+ 

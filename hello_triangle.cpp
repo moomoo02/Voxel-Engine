af5248313ee28a21c8f4a3da8fd4fdd93248cc11
Vince Nguyen <vincehnguyen@ucsb.edu>
2022-11-25 12:25:38 -0800

    Added window

--- hello_triangle.cpp
+++ hello_triangle.cpp
@@ -1,6 +1,14 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+
+
+//Takes in window, and new width and height. Changes viewport on resize
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{   
+    glViewport(0, 0, width, height);
+};
+
 //Shader in GLSL
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
@@ -9,14 +17,45 @@ const char *vertexShaderSource = "#version 330 core\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
 
-const char *fragmentShaderSource =
-"#version 400\n"
-"out vec4 frag_colour;"
-"void main() {"
-"  frag_colour = vec4(1.0f, 0.5f, 0.2f, 1.0);"
-"}";
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
 
 int main(){
+
+    //Initialize GLFW and configure using Hint
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    //Create a window object
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    //Make sure GLAD is initialized and working
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }     
+
+    //Specify viewport so opengl knows how to display data and coordinates with respect to the window
+    glViewport(0, 0, 800, 600);
+     
+    //Everytime window is resized, callback function to edit viewport.
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+
     //Vertices of a triangle
     GLfloat vertices[] = {
         -0.5f, -0.5f, 0.0f,
@@ -73,5 +112,34 @@ int main(){
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::VERTEX::LINKING_FAILED\n" << infoLog << std::endl;
     }
+
+    //Shaders will now be used, can delete
+    glUseProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);  
+
+    //Tell vertext buffer object how to interpret vertices
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);  
+
+    //Create a vertext array object to manage vertext attribiutes
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO); 
+    glBindVertexArray(VAO);
+
+
+    //Render loop
+    while(!glfwWindowShouldClose(window)) //Checks if GLFW has been instructed to close
+    {
+        //Draw Object
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        cout << "Object Drawn";
+        glfwSwapBuffers(window); //swap color buffer that is used to render 
+        glfwPollEvents(); //Checks if any events has been triggered, updates window states, and calls cooresponding functions
+    }
+
+    //Clean up all of GLFW resources
+    glfwTerminate();
+    return 0;
 }
  

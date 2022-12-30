32b6bf9851cdbe673fbc4a96a3e70ac772215ba6
Vince Nguyen <vincehnguyen@ucsb.edu>
2022-12-30 13:21:17 -0800

    Multiple cubes

--- main.cpp
+++ main.cpp
@@ -111,11 +111,18 @@ int main(){
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
-    //  GLfloat vertices2[] = {
-    //     0.5f, -0.5f, 0.0f,
-    //     1.5f, -0.5f, 0.0f,
-    //     0.0f,  0.5f, 0.0f
-    // };  
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f), 
+        glm::vec3( 2.0f,  5.0f, -15.0f), 
+        glm::vec3(-1.5f, -2.2f, -2.5f),  
+        glm::vec3(-3.8f, -2.0f, -12.3f),  
+        glm::vec3( 2.4f, -0.4f, -3.5f),  
+        glm::vec3(-1.7f,  3.0f, -7.5f),  
+        glm::vec3( 1.3f, -2.0f, -2.5f),  
+        glm::vec3( 1.5f,  2.0f, -2.5f), 
+        glm::vec3( 1.5f,  0.2f, -1.5f), 
+        glm::vec3(-1.3f,  1.0f, -1.5f)  
+    };
 
     //Parse shaders, compile, and link
     Shader shaderProgramClass("./Shaders.GLSL");
@@ -141,7 +148,7 @@ int main(){
 
     //Tell vertext buffer object how to interpret vertices
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)0);
-    glEnableVertexAttribArray(0);  
+    glEnableVertexAttribArray(0); 
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1); 
 
@@ -159,6 +166,7 @@ int main(){
 
     // Our state
     float blend = 0.0;
+    float fov = 45.0;
     glEnable(GL_DEPTH_TEST);  
 
     //Render loop
@@ -172,6 +180,7 @@ int main(){
         //Show demo window
         {
             ImGui::SliderFloat("Blend", &blend, 0.0f, 1.0f);
+            ImGui::SliderFloat("FOV", &fov, 0.0f, 180.0f);
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
         }
 
@@ -180,21 +189,26 @@ int main(){
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         //GLM Math
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(1.0f, 0.5f, 0.0f));
         glm::mat4 view = glm::mat4(1.0f);
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         glm::mat4 projection = glm::mat4(1.0f);
-        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-        shaderProgramClass.setMat4("model", model);
+        projection = glm::perspective(glm::radians(fov), 800.0f / 600.0f, 0.1f, 100.0f);
         shaderProgramClass.setMat4("view", view);
         shaderProgramClass.setMat4("projection", projection);
 
         //Draw Object
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
+        for(unsigned int i = 0; i < 10; i++)    
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * (i+1); 
+            model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            shaderProgramClass.setMat4("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
         shaderProgramClass.setFloat("blend", blend);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
 
         //Rendering
         ImGui::Render();

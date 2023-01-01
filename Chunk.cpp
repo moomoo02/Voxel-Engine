8644747145e47a4db260d4900845ea6802731baf
Vince Nguyen <vincehnguyen@ucsb.edu>
2023-01-01 14:57:47 -0800

    Chunk implementation skeleton

---
+++ Chunk.cpp
@@ -0,0 +1,21 @@
+#include "Chunk.h"
+
+Chunk::Chunk()
+{
+    
+}
+
+Chunk::~Chunk()
+{
+    
+}
+
+void Chunk::Update(float dt)
+{
+    
+}
+
+void Chunk::Render(OpenGLRenderer * pRenderer)
+{
+    
+}

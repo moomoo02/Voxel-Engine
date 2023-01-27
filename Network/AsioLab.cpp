655b8ec1694b97423282d7d846168e259548e282
Vince Nguyen <vincehnguyen@ucsb.edu>
2023-01-27 12:50:48 -0800

    net common header

--- Network/AsioLab.cpp
+++ Network/AsioLab.cpp
@@ -1,10 +1,4 @@
-#include <iostream>
-
-#define ASIO_STANDALONE 
-
-#include <asio.hpp>
-#include <asio/ts/buffer.hpp>
-#include <asio/ts/internet.hpp>
+#include "net_common.h"
 
 int main() {
     asio::error_code ec;
@@ -14,6 +8,20 @@ int main() {
 
     //Get the address of somewhere we wish to connect to
     asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 80);
+
+    //Create a socket
+    asio::ip::tcp::socket socket(context);
+
+    //Tell socket to connect
+    socket.connect(endpoint, ec);
+
+    if(!ec){
+        std::cout << "Connected!\n";
+    }else{
+        std::cout << "Failed to connect to address:\n" << ec.message() << '\n';
+    }
+
+ 
 
     std::cout << "Hello world\n";
     return 0;
